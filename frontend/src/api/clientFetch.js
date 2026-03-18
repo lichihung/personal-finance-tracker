6b5f0211b882cddec50bbf3350043633d9ba587b
lichihung <lichihun@umich.edu>
2026-03-17 17:46:05 -0700

    prepare frontend env for deployment

--- frontend/src/api/clientFetch.js
+++ frontend/src/api/clientFetch.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8000/api"
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api"
 const ACCESS_KEY = "access"
 const REFRESH_KEY = "refresh"
 
