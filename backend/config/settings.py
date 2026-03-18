3858bd3467fe2e4f44625264d8c9a2bf0fd3f6cd
lichihung <lichihun@umich.edu>
2026-03-17 17:27:35 -0700

    deploy backend setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ymsqwr+k(v#+zrs%4@rsbx917%ql#@*)qcp_8@hbt=1984_5(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
