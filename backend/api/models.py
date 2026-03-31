bde966e826a5a8b64a45a8528ffdd52dee8edcb3
lichihung <lichihun@umich.edu>
2026-03-30 17:16:33 -0700

    old account skip security

--- backend/api/models.py
+++ backend/api/models.py
@@ -38,7 +38,7 @@ class Transaction(models.Model):
     
 class UserSecurity(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    email_verified = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.user.username} security"
