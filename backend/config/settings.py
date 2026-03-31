a7ee4549305880db6dcd572e454ba3abbf4bba82
lichihung <lichihun@umich.edu>
2026-03-30 22:38:54 -0700

    fix Android app login

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://lichihung-finance-tracker.netlify.app",
+    "https://localhost",
     "http://localhost",
     "capacitor://localhost",
 ]
